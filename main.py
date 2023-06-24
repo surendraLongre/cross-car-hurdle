0f04a6c789fa1c3777d56e317b47f3e280f3e604
surendraLongre <murari.longre2366@gmail.com>
2023-06-24 16:32:25 +0530

    release 1.0.0
    finish with the Development

--- main.py
+++ main.py
@@ -5,10 +5,39 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=1200, height=1200)
 screen.tracer(0)
+player=Player()
 
 game_is_on = True
+
+# add key bindings
+screen.listen()
+screen.onkey(key="Up", fun=player.move_up)
+
+#initialize car module
+car=CarManager()
+
+#initialize scoreboard
+score=Scoreboard()
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.create_car()
+    car.move()
+
+    #detect collision with the car
+    for bus in car.all_cars:
+        if bus.distance(player) < 40:
+            game_is_on=False
+            score.game_over()
+    
+    #detect successful cross
+    if player.is_at_finish_line():
+        player.goto_start()
+        car.level_up()
+        score.score_up()
+
+
+screen.exitonclick()
